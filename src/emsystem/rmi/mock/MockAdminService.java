2af988e495497d259e313857a834a1d6901f1d4b
margine <ch_margine@163.com>
2014-05-25 22:23:24 +0800

    new Client B

---
+++ src/emsystem/rmi/mock/MockAdminService.java
@@ -0,0 +1,142 @@
+package emsystem.rmi.mock;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import emsystem.data.Course;
+import emsystem.data.Student;
+import emsystem.rmi.AdminService;
+
+public class MockAdminService implements AdminService {
+	@Override
+	public boolean adminLogin(String pAccount, String pPassword) {
+		if (pAccount.equals("admin") && pPassword.equals("admin")) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public ArrayList<Student> getStudents() {
+		ArrayList<Student> students = new ArrayList<Student>();
+		Student student1 = new Student("001", "哈哈", "女", getMajorName(),
+				"12334");
+		Student student2 = new Student("002", "呵呵", "男", getMajorName(), "11");
+
+		students.add(student1);
+		students.add(student2);
+		return students;
+	}
+
+	@Override
+	public boolean addStudent(Student pStudent) {
+		return true;
+	}
+
+	@Override
+	public boolean modifyStudent(Student pStudent) {
+		return true;
+	}
+
+	@Override
+	public boolean deleteStudent(String pStudentId) {
+		return true;
+	}
+
+	@Override
+	public ArrayList<Course> getCourses() {
+		ArrayList<Course> courses = new ArrayList<>();
+		Course course1 = new Course("001", "math", 3, "5-6", "mike", "china",
+				"是");
+		Course course2 = new Course("002", "art", 2, "1-2", "Tom", "China", "否");
+
+		courses.add(course1);
+		courses.add(course2);
+		return courses;
+	}
+
+	@Override
+	public String getMajorName() {
+
+		return "专业A";
+	}
+
+	@Override
+	public boolean addCourse(Course course) {
+		return true;
+	}
+
+	@Override
+	public boolean modifyCourse(Course course) {
+		return true;
+	}
+
+	@Override
+	public boolean deleteCourse(String pCourseId) {
+		return true;
+	}
+
+	@Override
+	public ArrayList<Student> getChoosedStudents(String pCourseId){
+		ArrayList<Student> students = new ArrayList<Student>();
+		if (pCourseId.equals("001")) {
+			Student student1 = new Student("001", "哈哈", "女", getMajorName(),
+					"12334");
+			Student student2 = new Student("002", "呵呵", "男", getMajorName(), "11");
+			
+			students.add(student1);
+			students.add(student2);
+		}
+		else {
+			Student student1 = new Student("003", "哈哈", "女", getMajorName(),
+					"12334");
+			Student student2 = new Student("004", "呵呵", "男", getMajorName(), "11");
+			
+			students.add(student1);
+			students.add(student2);
+		}
+
+		return students;
+	}
+	
+	
+	@Override
+	public ArrayList<Course> getUnsharedCourses(){
+		ArrayList<Course> courses = new ArrayList<>();
+		Course course1 = new Course("001", "math", 3, "5-6", "mike", "china",
+				"否");
+		Course course2 = new Course("002", "art", 2, "1-2", "Tom", "China", "否");
+
+		courses.add(course1);
+		courses.add(course2);
+		return courses;
+	}
+	
+	/**
+	 * 向总服务器提交结束跨专业选课请求
+	 */
+	@Override
+	public void postFinishChooseAction(){
+		
+	}
+	
+	/**
+	 * 提交共享学生信息请求
+	 * @throws RemoteException
+	 */
+	@Override
+	public void postShareStudentAction(){
+		
+	}
+	
+	/**
+	 * 提交共享课程信息请求
+	 * @throws RemoteException
+	 */
+	
+	@Override
+	public void postShareCourseAction(ArrayList<String> pCourseIds) {
+		
+	}
+	
+}
---
+++ src/emsystem/ui/student/StudentDropCoursePanel.java
@@ -0,0 +1,149 @@
+package emsystem.ui.student;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.table.TableColumn;
+
+import emsystem.data.Course;
+import emsystem.rmi.StudentServiceAdapter;
+import emsystem.ui.widget.CustomedTable;
+
+public class StudentDropCoursePanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7071721970040736891L;
+	/**
+	 * Create the panel.
+	 */
+	private String dropClassString = "确认退课";
+	private String[] dropClassColumnName = new String[] { "课程编号", "课程名称", "课时",
+			"学分", "授课老师", "授课地点", "退课" };
+	private int columnNums = 7;
+	private int idIndex = 0, nameIndex = 1, timeIndex = 2, creditIndex = 3,
+			teacherIndex = 4, addressIndex = 5, dropIndex = 6;
+
+	private JScrollPane scrollPane;
+	private CustomedTable mInfoTable;
+	private JButton dropButton;
+
+	private static int checkPosition = 6;
+
+	/**
+	 * Create the panel.
+	 */
+	private String mAccount;
+
+	public StudentDropCoursePanel(String pAccount) {
+		mAccount = pAccount;
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		scrollPane = new JScrollPane();
+
+		showTable();
+
+		dropButton = new JButton(dropClassString);
+		add(scrollPane);
+		add(dropButton);
+
+		addListeners();
+	}
+
+	private void showTable() {
+
+		mInfoTable = new CustomedTable(getData(), dropClassColumnName) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = -40319070391013926L;
+
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				if (column == checkPosition) {
+					return true;
+				}
+				return false;
+			}
+		};
+		TableColumn aColumn = mInfoTable.getColumnModel().getColumn(
+				checkPosition); // 设置第几列
+		aColumn.setCellEditor(mInfoTable.getDefaultEditor(Boolean.class));
+		aColumn.setCellRenderer(mInfoTable.getDefaultRenderer(Boolean.class));
+
+		scrollPane.setViewportView(mInfoTable);
+		scrollPane.invalidate();
+		scrollPane.repaint();
+		scrollPane.setPreferredSize(new Dimension(500, 350));
+
+	}
+
+	private void addListeners() {
+
+		dropButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (getCheckedIds().size() == 0) {
+					JOptionPane.showMessageDialog(null, "未选择任何课程");
+				} else {
+					doPost();
+					JOptionPane.showMessageDialog(null, "退课成功");
+				}
+			}
+		});
+
+	}
+
+	private Object[][] getData() {
+		StudentServiceAdapter adapter = StudentServiceAdapter.getInstance();
+		ArrayList<Course> courses = adapter.getCoursesToDrop(mAccount);
+		Object[][] data = new Object[][] {};
+		if (courses != null) {
+			data = new Object[courses.size()][columnNums];
+			int i = 0;
+			Iterator<Course> iterator = courses.iterator();
+			while (iterator.hasNext()) {
+				Course course = iterator.next();
+				data[i][idIndex] = course.getId();
+				data[i][nameIndex] = course.getCourseName();
+				data[i][timeIndex] = course.getCourseTime();
+				data[i][creditIndex] = course.getCredit();
+				data[i][teacherIndex] = course.getTeacher();
+				data[i][addressIndex] = course.getAddress();
+				data[i][dropIndex] = false;
+				i++;
+			}
+
+		}
+		return data;
+	}
+
+	private ArrayList<String> getCheckedIds() {
+		ArrayList<String> indexes = new ArrayList<>();
+		for (int i = 0; i < getData().length; i++) {
+			if (((Boolean) mInfoTable.getValueAt(i, checkPosition))
+					.booleanValue()) {// 选中
+				indexes.add((String) mInfoTable.getValueAt(i, idIndex));
+			}
+		}
+		return indexes;
+	}
+
+	private void doPost() {
+		StudentServiceAdapter adapter = StudentServiceAdapter.getInstance();
+		adapter.dropCourses(mAccount, getCheckedIds());
+		
+		//refresh
+		showTable();
+	}
+}
