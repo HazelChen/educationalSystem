6fa0f221e8a192df0568ed10dddcb0977b60f446
Hazel <cfzljcl@gmail.com>
2014-05-26 17:38:54 +0800

    config

--- EMSystemServerB/src/emsystem/xml/ConfigConstant.java
+++ EMSystemServerB/src/emsystem/xml/ConfigConstant.java
@@ -4,8 +4,8 @@ public class ConfigConstant {
 	public static final String COURSE_ROOT = "courses";
 	public static final String COURSE_ELEMENT = "course";
 	
-	public static final String ELECTIVE_ROOT = "choises";
-	public static final String ELECTIVE_ELEMENT = "choise";
+	public static final String ELECTIVE_ROOT = "choices";
+	public static final String ELECTIVE_ELEMENT = "choice";
 	
 	public static final String STUDENT_ROOT = "students";
 	public static final String STUDENT_ELEMENT = "student";
